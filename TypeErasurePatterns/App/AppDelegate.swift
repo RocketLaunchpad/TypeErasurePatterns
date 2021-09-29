9d1d7271b2d5cb306bb44abf42071aa1d1a03f95
Paul Calnan <pwc3@users.noreply.github.com>
2021-09-29 10:05:08 -0400

    Workspace setup

---
+++ TypeErasurePatterns/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TypeErasurePatterns
+//
+//  Created by Paul Calnan on 9/29/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
